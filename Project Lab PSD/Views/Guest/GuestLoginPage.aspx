a2121269afe68b9756d0c4d97a3a373e79513359
Ger706 <frostggwp706@gmail.com>
2024-06-12 08:18:14 +0700

    #Guest Page

--- Project Lab PSD/Views/Guest/GuestLoginPage.aspx
+++ Project Lab PSD/Views/Guest/GuestLoginPage.aspx
@@ -2,4 +2,13 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentBody" runat="server">
+
+     <asp:Label ID="lblUserName" runat="server" Text="Username:"></asp:Label>
+            <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+            <asp:Label ID="lblPassword" runat="server" Text="Password:"></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+           <asp:Button ID="Button1" runat="server" Text="Login" OnClick="Button1_Click"/>
+            <asp:Label ID="lblMessage" runat="server"></asp:Label>
+
+
 </asp:Content>
--- Project Lab PSD/Views/Guest/GuestLoginPage.aspx.cs
+++ Project Lab PSD/Views/Guest/GuestLoginPage.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Project_Lab_PSD.Handlers;
+using Project_Lab_PSD.Models;
+using Project_Lab_PSD.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +12,27 @@ namespace Project_Lab_PSD.Views.Guest
 {
     public partial class LoginPage : System.Web.UI.Page
     {
+      
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string userName = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            GuestHandler guestHandler = new GuestHandler();
+            Response<MsUser> response = guestHandler.Login(userName, password);
+
+            if (response.IsSuccess == false)
+            {
+                lblMessage.Text = response.Message;
+            } else
+            {
+                Response.Redirect("~/Views/Guest/GuestStationeryPage.aspx");
+            }
 
         }
     }
