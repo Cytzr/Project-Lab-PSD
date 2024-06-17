48f3d9aa86ede10133d49e6918be386598992e23
Kelvin Ethanael Yahja <kelvin.yahja@binus.edu>
2024-06-17 21:36:51 +0700

    feat: added model

---
+++ Project Lab PSD/Models/TransactionDetailWithStationery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_Lab_PSD.Models
+{
+    public class TransactionDetailWithStationery
+    {
+        public int TransactionID { get; set; }
+        public int Quantity { get; set; }
+        public int StationeryID { get; set; }
+        public string StationeryName { get; set; }
+        public int StationeryPrice { get; set; }
+
+    }
+}
